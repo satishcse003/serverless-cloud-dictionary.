320cf537af74d609ac1693940ea5c779fe9ad93f
satish <satishcse003@gmail.com>
2025-05-19 10:48:17 +0530

    Integrate API with React front-end

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredTerms, setFilteredTerms] = useState([]);
 
-  const apiUrl = '<ENTER_YOUR_API_URL>'; // Replace with your API Gateway URL
+  const apiUrl = 'https://ccjwq8vzza.execute-api.ap-south-1.amazonaws.com/dev'; // Replace with your API Gateway URL
 
   const handleSearch = () => {
     console.log('Fetching data from API...');
